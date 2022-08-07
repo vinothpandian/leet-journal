4bdb0cc5ad23c7c9aca103f2ce6bdf16c11f4daf
Vinoth Pandian <vinothpandian@users.noreply.github.com>
2022-08-07 10:04:43 -0400

    add review update

--- src/routes/learn/index.svelte
+++ src/routes/learn/index.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
+	import AddReviewForm from '$components/learn/AddReviewForm.svelte';
+
 	import Filters from '$components/learn/Filters.svelte';
 	import ProblemsList from '$components/learn/ProblemsList.svelte';
 	import SearchBar from '$components/learn/SearchBar.svelte';
 	import TitleBar from '$components/learn/TitleBar.svelte';
 	import type { ProblemState, ReviewInfo } from '$components/learn/types';
+	import { addReviews } from '$db/queries';
 	import type { Hardness } from '$lib/types';
 	import {
 		hasMoreProblems,
@@ -11,6 +14,7 @@
 		problems,
 		updateFilters,
 	} from '$store/problems';
+	import toast from 'svelte-french-toast';
 
 	let selectedProblems: Record<number, boolean> = {};
 
@@ -18,36 +22,41 @@
 	let hardness: Hardness | '' = '';
 	let tag: string = '';
 
+	let someProblemSelected = false;
 	let showReviewPopup = false;
 
 	function clearSelected() {
 		selectedProblems = {};
+		showReviewPopup = false;
 	}
 
 	function handleChange(event: CustomEvent<ProblemState>) {
 		const { problemId, checked } = event.detail;
 		selectedProblems[problemId] = checked;
+
+		someProblemSelected = Object.values(selectedProblems).some(
+			(checked) => checked
+		);
+		showReviewPopup = someProblemSelected;
+	}
+
+	function handleClose() {
+		showReviewPopup = false;
 	}
 
 	async function handleAddReview(event: CustomEvent<ReviewInfo>) {
 		const { difficulty, reviewDate } = event.detail;
 		const problemIds = Object.keys(selectedProblems).map((id) => Number(id));
 		showReviewPopup = false;
-		// await addReviews(reviewDate, difficulty, problemIds);
-		// toast.success('Reviews added..');
+		await addReviews(reviewDate, difficulty, problemIds);
+		toast.success('Reviews added..');
 	}
-
-	$: someProblemSelected = Object.values(selectedProblems).some(
-		(checked) => checked
-	);
-
-	$: someProblemSelected, (showReviewPopup = true);
 
 	$: searchTerm, tag, hardness, updateFilters({ searchTerm, tag, hardness });
 </script>
 
 <div class="flex flex-col gap-6 p-6 wrapper">
-	<TitleBar on:clearSelected={clearSelected} />
+	<TitleBar {someProblemSelected} on:clearSelected={clearSelected} />
 	<SearchBar bind:searchTerm />
 	<Filters bind:hardness bind:tag />
 	<ProblemsList
@@ -57,9 +66,9 @@
 		on:change={handleChange}
 		on:loadMore={loadNextPage}
 	/>
-	<!-- {#if showReviewPopup}
-		<AddReviewForm on:save={handleAddReview} />
-	{/if} -->
+	{#if showReviewPopup}
+		<AddReviewForm on:save={handleAddReview} on:close={handleClose} />
+	{/if}
 </div>
 
 <style>
