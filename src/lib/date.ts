0a87d9e70c5c55b9f1a8867d4ed57b9d5f3b9d64
Vinoth Pandian <vinothpandian@users.noreply.github.com>
2022-08-17 00:44:06 -0400

    add review item page

--- src/lib/date.ts
+++ src/lib/date.ts
@@ -1,9 +1,29 @@
 import dayjs from 'dayjs/esm';
+import LocalizedFormat from 'dayjs/esm/plugin/localizedFormat';
+import type { Review } from './types';
+
+dayjs.extend(LocalizedFormat);
 
 const relativeDateMap = ['today', 'yesterday', 'day before yesterday'];
 
-export const getRelativeDate = (date: string) => {
-	const dayDifference = dayjs(date).diff(new Date(), 'days');
+export const getDaysPassedCount = (date: string) =>
+	dayjs(date).diff(new Date(), 'days');
 
-	return relativeDateMap?.[dayDifference] ?? `${dayDifference} days ago`;
+export const getRelativeDate = (date: string) => {
+	const daysPassed = Math.abs(getDaysPassedCount(date));
+
+	return relativeDateMap?.[daysPassed] ?? `${daysPassed} days ago`;
 };
+
+export const getFormattedDate = (date: string) => dayjs(date).format('ll');
+
+export const sortReviewsByDate = (
+	reviews: Review[],
+	order: 'asc' | 'desc' = 'desc'
+) =>
+	reviews.sort(
+		({ reviewDate: firstReviewDate }, { reviewDate: secondReviewDate }) =>
+			(order === 'asc' ? 1 : -1) *
+			(getDaysPassedCount(firstReviewDate) -
+				getDaysPassedCount(secondReviewDate))
+	);
--- src/routes/review/[id]/+page.svelte
+++ src/routes/review/[id]/+page.svelte
@@ -1,7 +1,110 @@
 <script lang="ts">
-	export let data: any;
+	import HardnessBadge from '$components/common/HardnessBadge.svelte';
+	import LeetCodeLink from '$components/common/LeetCodeLink.svelte';
+	import { updateProblemNotes } from '$db/queries';
+
+	import { getColorForPercentage } from '$lib/colors';
+	import {
+		getFormattedDate,
+		getRelativeDate,
+		sortReviewsByDate,
+	} from '$lib/date';
+	import { getDifficultyText } from '$lib/difficulty';
+
+	import { getRetention } from '$lib/retention';
+	import type { Problem } from '$lib/types';
+	import dayjs from 'dayjs/esm';
+	import LocalizedFormat from 'dayjs/esm/plugin/localizedFormat';
+	dayjs.extend(LocalizedFormat);
+
+	export let data: { problem: Problem };
 
 	const { problem } = data;
+
+	let notes: string = problem?.notes ?? '';
+
+	const handleNotesChange = async () => {
+		await updateProblemNotes(problem.id, notes);
+	};
+
+	$: lastReview = problem?.reviews?.[0] ?? {
+		reviewDate: new Date(),
+		difficulty: 2,
+	};
+	$: retention = getRetention(lastReview);
+	$: retentionColor = getColorForPercentage(retention - 1);
 </script>
 
-{JSON.stringify(problem)}
+<div class="flex flex-col gap-6 p-6 wrapper">
+	{#if problem}
+		<div class="flex gap-4">
+			<div
+				class="radial-progress text-2xs"
+				style={`--value:${retention};--size: 3rem; color:${retentionColor};`}
+			>
+				{retention}%
+			</div>
+			<div class="grid flex-grow gap-2">
+				<div class="inline-flex justify-between">
+					<span>{problem.title}</span>
+					<span class="inline-flex gap-2">
+						<HardnessBadge hardness={problem.hardness} />
+						<LeetCodeLink titleSlug={problem.titleSlug} />
+					</span>
+				</div>
+				<div class="inline-flex justify-between">
+					<span class="text-slate-400 font-light text-xs">
+						Reviewed {getRelativeDate(problem.reviews[0].reviewDate)}
+					</span>
+				</div>
+			</div>
+		</div>
+		<textarea
+			class="textarea textarea-bordered w-full mt-6"
+			placeholder="Notes.."
+			rows="10"
+			bind:value={notes}
+			on:change={handleNotesChange}
+		/>
+		<div class="flex flex-col md:flex-row w-full">
+			<div class="stat place-items-center">
+				<div class="stat-title text-sm">Recent Review Date</div>
+				<div class="stat-value text-2xl">
+					{getFormattedDate(lastReview.reviewDate)}
+				</div>
+			</div>
+			<div class="stat place-items-center">
+				<div class="stat-title text-sm">Recent Review Difficulty</div>
+				<div class="stat-value text-2xl">
+					{getDifficultyText(lastReview.difficulty)}
+				</div>
+			</div>
+		</div>
+		<ul class="steps steps-vertical	flex-grow overflow-auto">
+			{#each sortReviewsByDate(problem.reviews) as { reviewDate, difficulty }}
+				<li data-content="" class="step">
+					<div>
+						<span>Marked as </span>
+						<span class={`difficulty-${difficulty}`}>
+							{getDifficultyText(difficulty)}
+						</span>
+						<span>on {getFormattedDate(reviewDate)}</span>
+					</div>
+				</li>
+			{/each}
+		</ul>
+	{/if}
+</div>
+
+<style>
+	.wrapper {
+		height: calc(100vh - var(--navbar-height) - var(--sidenavbar-height));
+		overflow: hidden;
+	}
+
+	@media (min-width: 768px) {
+		.wrapper {
+			height: calc(100vh - var(--navbar-height));
+		}
+	}
+</style>
