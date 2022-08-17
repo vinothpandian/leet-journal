ad9f2afcc20ad951a832431b0ac368db84ac07e5
Vinoth Pandian <vinothpandian@users.noreply.github.com>
2022-08-17 00:11:24 -0400

    add back to side nav

--- src/components/common/SidebarNavigation.svelte
+++ src/components/common/SidebarNavigation.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { page } from '$app/stores';
+	import GoBack from '$icons/GoBack.svelte';
 	import Settings from '$icons/Settings.svelte';
 	let path: string;
 
@@ -9,13 +10,21 @@
 <div
 	class="flex justify-between items-center p-4 w-full border-b border-gray-100"
 >
-	<div class="tabs bg-base-100">
-		<a
-			href="/"
-			class="tab"
-			class:tab-active={path === '/' || path.startsWith('/review')}>Review</a
-		>
-		<a href="/learn" class="tab" class:tab-active={path === '/learn'}>Learn</a>
+	<div class="flex flex-grow items-center">
+		{#if path.startsWith('/review/')}
+			<a href="/" class="btn btn-link btn-xs btn-square rounded">
+				<GoBack />
+			</a>
+		{/if}
+		<div class="tabs bg-base-100">
+			<a
+				href="/"
+				class="tab"
+				class:tab-active={path === '/' || path.startsWith('/review')}>Review</a
+			>
+			<a href="/learn" class="tab" class:tab-active={path === '/learn'}>Learn</a
+			>
+		</div>
 	</div>
 	<a href="/settings" class="btn btn-link btn-xs btn-square rounded">
 		<Settings />
