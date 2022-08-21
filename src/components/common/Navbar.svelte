fd58f61f1d50be85931b5460b4a68b6ab79717ea
Vinoth Pandian <vinothpandian@users.noreply.github.com>
2022-08-21 15:02:16 -0400

    add help tooltip

--- src/components/common/Navbar.svelte
+++ src/components/common/Navbar.svelte
@@ -1,3 +1,15 @@
-<div class="navbar bg-base-100 drop-shadow-sm">
+<script>
+	import Help from '$icons/Help.svelte';
+</script>
+
+<div class="navbar bg-base-100 drop-shadow-sm flex flex-nowrap justify-between">
 	<a href="/" class="btn btn-ghost normal-case text-xl">Leet Journal</a>
+	<div
+		class="tooltip tooltip-bottom"
+		data-tip="Spaced repetition is an evidence-based learning technique that incorporates increasing intervals of time between subsequent review of previously learned material in order to exploit the psychological spacing effect."
+	>
+		<div class="btn btn-link btn-xs btn-square rounded text-gray-800">
+			<Help />
+		</div>
+	</div>
 </div>
